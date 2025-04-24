4e7f2d84f2746ea4b0f55ac1b10cb774133889c4
cuongnc0211 <cuongnguyenfu@gmail.com>
2025-04-24 20:51:33 +0700

    login logout spec

--- spec/integration/authentication_spec.rb
+++ spec/integration/authentication_spec.rb
@@ -1,34 +1,51 @@
 # spec/integration/api_spec.rb
-# rswag:specs:swaggerize
+# rails rswag:specs:swaggerize
 require 'swagger_helper'
 
 RSpec.describe 'API V1', type: :request do
-  # path '/api/v1/login' do
-  #   post 'User login' do
-  #     tags 'Authentication'
-  #     consumes 'application/json'
+  path '/users/tokens/sign_in' do
+    post 'User login' do
+      tags 'Authentication'
+      consumes 'application/json'
 
-  #     parameter name: 'payload', in: :body, schema: {
-  #       type: :object,
-  #       properties: {
-  #         email: { type: :string },
-  #         password: { type: :string }
-  #       },
-  #       required: [ 'email', 'password' ]
-  #     }
+      parameter name: 'payload', in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string }
+        },
+        required: [ 'email', 'password' ]
+      }
 
-  #     response '200', 'successful login' do
-  #       let(:user) { create(:user) }
-  #       let(:payload) { {email: user.email, password: 'password'} }
+      response '200', 'successful login' do
+        let(:user) { create(:user) }
+        let(:payload) { {email: user.email, password: 'password'} }
         
-  #       run_test!
-  #     end
+        run_test!
+      end
 
-  #     response '401', 'invalid credentials' do
-  #       let(:payload) { { email: 'wrong@example.com', password: 'wrong' } }
+      response '400', 'invalid credentials' do
+        let(:payload) { { email: 'wrong@example.com', password: 'wrong' } }
 
-  #       run_test!
-  #     end
-  #   end
-  # end
+        run_test!
+      end
+    end
+  end
+
+  path '/users/tokens/revoke' do
+    let!(:user) { create(:user) }
+    let!(:token) { create(:devise_api_token, resource_owner: user, access_token: 'authen_token') }
+
+    post 'User logout' do
+      tags 'Authentication'
+      consumes 'application/json'
+      parameter name: 'Authorization', in: :header
+
+      response '204', 'successful logout' do
+        let(:Authorization) { 'Bearer authen_token' }
+        
+        run_test!
+      end
+    end
+  end
 end
--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -3,28 +3,12 @@
 require 'swagger_helper'
 
 RSpec.describe 'API V1', type: :request do
-  # path '/api/v1/logout' do
-  #   let!(:user) { create(:user, auth_token: 'authen_token') }
-
-  #   post 'User logout' do
-  #     tags 'Authentication'
-  #     consumes 'application/json'
-  #     parameter name: 'Authorization', in: :header
-
-  #     response '200', 'successful logout' do
-  #       let(:Authorization) { 'Bearer authen_token' }
-        
-  #       run_test!
-  #     end
-  #   end
-  # end
-
   path '/api/v1/users/profile' do
     let(:user) { create(:user) }
     let!(:token) { create(:devise_api_token, resource_owner: user, access_token: 'authen_token') }
 
     get 'get profile' do
-      tags 'Get Profile'
+      tags 'Users APIs'
       consumes 'application/json'
       parameter name: 'Authorization', in: :header
 
